ae388dc0a5783f28ec3199a6c0fcaea427cb141f
Hyunjun-11 <mical0108@gmail.com>
2024-05-10 17:50:31 +0900

    feat : Member Entity

--- src/main/java/com/church/domain/members/entity/Member.java
+++ src/main/java/com/church/domain/members/entity/Member.java
@@ -1,4 +1,44 @@
 package com.church.domain.members.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String church;
+
+    @Column
+    private String position;
+
+    @Column
+    private String email;
+    @Column
+    private String phoneNumber;
+
+
+
+
 }
