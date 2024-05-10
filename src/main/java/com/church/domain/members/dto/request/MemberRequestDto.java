90e1a5dff09c20acc07235b6546709749f9f216a
Hyunjun-11 <mical0108@gmail.com>
2024-05-10 17:51:08 +0900

    feat : MemberDto

--- src/main/java/com/church/domain/members/dto/request/MemberRequestDto.java
+++ src/main/java/com/church/domain/members/dto/request/MemberRequestDto.java
@@ -1,4 +1,35 @@
 package com.church.domain.members.dto.request;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class MemberRequestDto {
+    @NotEmpty(message = "아이디를 입력해주세요")
+    private String memberId;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[a-z\\d!@#$%^&*()_-]{5,12}$", message = "비밀번호는 5~12자 이내 영어(소문자),숫자,특수기호(선택) 범위에서 입력해야합니다.")
+    private String password;
+
+    private String name;
+
+    private String church;
+
+    private String position;
+
+    @Email
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "올바른 형식의 이메일을 입력해주세요.")
+    private String email;
+
+    private String phoneNumber;
+
+
 }
--- src/main/java/com/church/domain/members/dto/response/MemberResponseDto.java
+++ src/main/java/com/church/domain/members/dto/response/MemberResponseDto.java
@@ -1,4 +1,25 @@
 package com.church.domain.members.dto.response;
 
+
+import com.church.domain.members.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
 public class MemberResponseDto {
+
+    private String memberId;
+    private String name;
+    private String church;
+    private String position;
+    private String email;
+
+    public MemberResponseDto(Member member) {
+        this.memberId = member.getMemberId();
+        this.name = member.getName();
+        this.church =member.getChurch();
+        this.position = member.getPosition();
+        this.email = member.getEmail();
+    }
 }
