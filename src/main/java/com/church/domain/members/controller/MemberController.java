d31d0ca42f50e1db4ccfc1ad69b9caf6b9cf835b
Hyunjun-11 <mical0108@gmail.com>
2024-05-10 17:50:38 +0900

    feat : Member Controller

--- src/main/java/com/church/domain/members/controller/MemberController.java
+++ src/main/java/com/church/domain/members/controller/MemberController.java
@@ -1,14 +1,47 @@
 package com.church.domain.members.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.church.domain.members.dto.request.MemberRequestDto;
+import com.church.domain.members.dto.response.MemberResponseDto;
+import com.church.domain.members.service.MemberService;
+import com.church.domain.members.dto.request.SignInRequestDto;
+import com.church.util.message.Message;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("api/member")
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/")
     public String index() {
         return "Hello World";
+    }
+
+    @GetMapping("/readAll")
+    public ResponseEntity<Message<List<MemberResponseDto>>> readAll() {
+        return memberService.readAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Message<MemberResponseDto>> findById(@PathVariable Long id) {
+        return memberService.readOne(id);
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<Message<MemberResponseDto>> create(@Valid @RequestBody MemberRequestDto requestDto) {
+        return memberService.signUp(requestDto);
+    }
+    @PostMapping("/signIn")
+    public ResponseEntity<Message<MemberResponseDto>> signIn(@Valid @RequestBody SignInRequestDto requestDto) {
+        return memberService.signIn(requestDto);
+
     }
 }
