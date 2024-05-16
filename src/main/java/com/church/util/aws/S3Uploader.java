c114bc4f4cc1614a6b646412cba07e51c30f074c
Hyunjun-11 <mical0108@gmail.com>
2024-05-16 22:57:00 +0900

    feat : AWS s3 uploader

--- src/main/java/com/church/util/aws/S3Uploader.java
+++ src/main/java/com/church/util/aws/S3Uploader.java
@@ -1,6 +1,11 @@
 package com.church.util.aws;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,6 +15,11 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     public String upload(MultipartFile multipartFile) throws IOException {
 
