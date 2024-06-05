2098ed8ae726d2051aa7a7130a5b560e38dcb62a
Hyunjun-11 <mical0108@gmail.com>
2024-06-05 18:51:01 +0900

    chore : gradle.yml CD edit

--- src/main/java/com/church/security/config/CustomSecurityConfig.java
+++ src/main/java/com/church/security/config/CustomSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.church.security.config;
 
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
