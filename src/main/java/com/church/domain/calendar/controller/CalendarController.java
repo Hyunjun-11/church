4d7798940719395a215d22c5d369396cd00d8ca4
Hyunjun-11 <mical0108@gmail.com>
2024-06-09 18:58:24 +0900

    feat :calendar controller

--- src/main/java/com/church/domain/calendar/controller/CalendarController.java
+++ src/main/java/com/church/domain/calendar/controller/CalendarController.java
@@ -1,16 +1,41 @@
 package com.church.domain.calendar.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.church.domain.calendar.dto.CalendarRequestDto;
+import com.church.domain.calendar.dto.CalendarResponseDto;
+import com.church.domain.calendar.service.CalendarService;
+import com.church.util.message.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/calendar")
+@RequiredArgsConstructor
 public class CalendarController {
+
+    private final CalendarService calendarService;
 
     @GetMapping("/test")
     public String index() {
         return "캘린더테스트";
     }
-    
+
+    @GetMapping("/")
+    public ResponseEntity<Message<CalendarResponseDto>> realAll() {
+        return calendarService.realAll();
+    }
+    @PostMapping("/")
+    public ResponseEntity<Message<CalendarResponseDto>> saveCalendar(@RequestBody CalendarRequestDto calendarRequestDto) {
+        return calendarService.saveCalendar(calendarRequestDto);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Message<CalendarResponseDto>> updateCalendar(@PathVariable Long id, @RequestBody CalendarRequestDto calendarRequestDto) {
+        return calendarService.updateCalendar(id,calendarRequestDto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Message<CalendarResponseDto>> deleteCalendar(@PathVariable Long id) {
+        return calendarService.deleteCalendar(id);
+
+    }
+
 }
