30731319268cef48d2a9c87f19589747515d9459
Hyunjun-11 <mical0108@gmail.com>
2024-06-09 18:58:31 +0900

    feat :calendar service

---
+++ src/main/java/com/church/domain/calendar/service/CalendarService.java
@@ -0,0 +1,37 @@
+package com.church.domain.calendar.service;
+
+import com.church.domain.calendar.dto.CalendarRequestDto;
+import com.church.domain.calendar.dto.CalendarResponseDto;
+import com.church.util.message.Message;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CalendarService {
+
+    public ResponseEntity<Message<CalendarResponseDto>> realAll() {
+        return null;
+    }
+
+    //일정추가
+    @Transactional
+    public ResponseEntity<Message<CalendarResponseDto>> saveCalendar(CalendarRequestDto calendarRequestDto) {
+        return null;
+    }
+
+    //일정수정
+    @Transactional
+    public ResponseEntity<Message<CalendarResponseDto>> updateCalendar(Long id, CalendarRequestDto calendarRequestDto) {
+        return null;
+    }
+
+    //일정 삭제
+    @Transactional
+    public ResponseEntity<Message<CalendarResponseDto>> deleteCalendar(Long id) {
+        return null;
+    }
+
+}
