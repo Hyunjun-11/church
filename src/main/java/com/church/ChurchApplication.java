232ee9213ae529944c724a81fc8d523df262d126
Hyunjun-11 <mical0108@gmail.com>
2024-06-10 16:43:06 +0900

    chore : application @EnableJpaAuditing add

--- src/main/java/com/church/ChurchApplication.java
+++ src/main/java/com/church/ChurchApplication.java
@@ -2,8 +2,10 @@ package com.church;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChurchApplication {
 
     public static void main(String[] args) {
