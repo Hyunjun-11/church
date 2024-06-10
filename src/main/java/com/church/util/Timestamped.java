1fcbda13c1cfc41dd2c2a2c21e2bbb821ec308d5
Hyunjun-11 <mical0108@gmail.com>
2024-06-10 15:42:27 +0900

    fix : Timestamped superBuilder fix

--- src/main/java/com/church/util/Timestamped.java
+++ src/main/java/com/church/util/Timestamped.java
@@ -5,6 +5,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
+@SuperBuilder
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
