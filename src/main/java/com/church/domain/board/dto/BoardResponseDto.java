3ec70d9b8951dbfe60d900fccb85a283b8b91bef
Hyunjun-11 <mical0108@gmail.com>
2024-06-10 16:24:55 +0900

    chore : BoardResponseDto createAt add

--- src/main/java/com/church/domain/board/dto/BoardResponseDto.java
+++ src/main/java/com/church/domain/board/dto/BoardResponseDto.java
@@ -5,10 +5,15 @@ import com.church.domain.calendar.dto.CalendarDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class BoardResponseDto extends BoardDto{
     private Long boardId;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
 
     public BoardResponseDto(Board board) {
         super(
@@ -16,6 +21,10 @@ public class BoardResponseDto extends BoardDto{
                 board.getContent(),
                 board.getAuthor());
 
+
         this.boardId = board.getId();
+        this.createAt = board.getCreateAt();
+        this.updateAt = board.getModifiedDate();
+
     }
 }
