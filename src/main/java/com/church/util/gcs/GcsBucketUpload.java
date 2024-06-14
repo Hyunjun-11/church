21bbb293bab123c2ec1a70ce5623fdde26b5ab4c
Hyunjun-11 <mical0108@gmail.com>
2024-06-14 15:09:27 +0900

    chore : bucket json CICD v0.1

--- src/main/java/com/church/util/gcs/GcsBucketUpload.java
+++ src/main/java/com/church/util/gcs/GcsBucketUpload.java
@@ -1,7 +1,6 @@
 package com.church.util.gcs;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
